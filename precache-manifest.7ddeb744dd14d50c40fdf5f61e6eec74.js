88f8055b0d5203990d2dfa22ced02051bdd73f23
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-03-24 23:17:17 +0400

    Updates

---
+++ precache-manifest.7ddeb744dd14d50c40fdf5f61e6eec74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f9868486ec36fccf36f",
+    "url": "/nour-s/resume/static/js/runtime~main.48155025.js"
+  },
+  {
+    "revision": "a0ff88fe2c0ccb593f31",
+    "url": "/nour-s/resume/static/js/main.1afbe0a1.chunk.js"
+  },
+  {
+    "revision": "f05a1df3217379e807f2",
+    "url": "/nour-s/resume/static/js/2.fba19b92.chunk.js"
+  },
+  {
+    "revision": "a0ff88fe2c0ccb593f31",
+    "url": "/nour-s/resume/static/css/main.b251742a.chunk.css"
+  },
+  {
+    "revision": "a1f89a07a1828b659c4ad12dba533720",
+    "url": "/nour-s/resume/index.html"
+  }
+];
