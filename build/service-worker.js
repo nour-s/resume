e9fa75d500c06244ae1bae2af3ed765acc257642
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-03-24 23:36:43 +0400

    feat: Added gh-pages package - Update the build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d15f1266c287df524bfe8816d401abf4.js"
+  "/resume/precache-manifest.eb4b91eac9a6e03b941843ac8cdb6c34.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
